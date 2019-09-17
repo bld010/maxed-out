52297ce140fdc226df4acb4fa7339f38a27ea08f
Brianna DelValle <briannadelvalle@gmail.com>
2019-09-16 23:12:40 -0600

    Write tests for pacContributions reducer

--- src/reducers/pacContributions.test.js
+++ src/reducers/pacContributions.test.js
@@ -1,7 +1,34 @@
-import React, { shallow } from 'react';
+import { pacContributions } from './pacContributions';
 
 describe('pacContributions reducer', () => {
-  it('should be true', () => {
-    expect(true).toEqual(true)
+  it(`should return an empty array if action type does not match \n
+      and state is undefined`, () => {
+    let mockState = undefined;
+    let mockAction = {
+      type: 'BLAH',
+      pacContributions: [1,2,3]
+    };
+    
+    expect(pacContributions(mockState, mockAction)).toEqual([]);
+  })
+
+  it('should return the state if action type does not match', () => {
+    let mockState = [1, 2, 3, 4];
+    let mockAction = {
+      type: 'BLAH BLAH',
+      pacContributions: [5, 6, 7, 8]
+    };
+
+    expect(pacContributions(mockState, mockAction)).toEqual(mockState)
+  })
+
+  it('should return an array of contributions if type matches', () => {
+    let mockState = [1,2,3,4];
+    let mockAction = {
+      type: 'SET_PAC_CONTRIBUTIONS',
+      pacContributions: [4, 5, 6, 7]
+    };
+
+    expect(pacContributions(mockState, mockAction)).toEqual([4, 5, 6, 7]);
   })
 })
