6c4c219efede5ac9e76805ab36c9b046c05993f1
Brianna DelValle <briannadelvalle@gmail.com>
2019-09-16 23:04:52 -0600

    Write tests for candidate reducer

--- src/reducers/candidate.test.js
+++ src/reducers/candidate.test.js
@@ -1,7 +1,26 @@
-import React, { shallow } from 'react';
+import { candidate } from './candidate'
 
 describe('candidate Reducer', () => {
-  it('should be true', () => {
-    expect(true).toEqual(true)
+  it('should return state if action does not match SET_CURRENT_CANDIDATE', () => {
+    let mockState = {name: 'Book'}
+    let mockAction = {type: 'BlahBlah', candidate: {name:'Lamp'}}
+
+    expect(candidate(mockState, mockAction)).toEqual(mockState)
+  })
+
+  it(`should return the candidate object if action type matches and \n
+      there is a candidate property in the action`, () =>{
+    let mockState = {name: 'Book'}
+    let mockAction = {type: 'SET_CURRENT_CANDIDATE', candidate: {name:'Lamp'}}
+
+    expect(candidate(mockState, mockAction)).toEqual({name: 'Lamp'})
+  })
+
+  it(`should return null if no state is provided, action type doen not match, \n
+      and there is no candidate property in the action`, () => {
+    let mockAction = {type: 'BlahBlah', committee_id: {name:'Lamp'}}
+
+    expect(candidate(undefined, mockAction)).toEqual(null)
+
   })
 })
